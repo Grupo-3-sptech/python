da7ce6ad530bc0e247fc7f18d3a274a23c11d4ea
GabrielBoos22 <gabriel.bduarte@sptech.school>
2023-10-20 18:13:53 -0300

    Update captacao.py

--- captacao.py
+++ captacao.py
@@ -14,17 +14,40 @@ def mysql_connection(host, user, passwd, database=None):
     )
     return connection
 
+def bytes_para_gb(bytes_value):
+    return bytes_value / (1024 ** 3)
+
 connection = mysql_connection('localhost', 'root', '5505', 'MedConnect')
 
 while True:
-    memoriaPorcentagem = psutil.virtual_memory()[2]
+
+    #CPU
     cpuPorcentagem = psutil.cpu_percent(None)
+    frequenciaCpuMhz = psutil.cpu_freq(percpu=False)
+    cpuVelocidadeEmGhz = "{:.2f}".format(frequenciaCpuMhz.current / 1000)
+    tempoSistema = psutil.cpu_times()[1] 
+    processos = len(psutil.pids())
+
+    
+    #Memoria
+    memoriaPorcentagem = psutil.virtual_memory()[2]
+    memoriaTotal = "{:.2f}".format(bytes_para_gb(psutil.virtual_memory().total))
+    memoriaUsada = "{:.2f}".format(bytes_para_gb(psutil.virtual_memory().used))
+    memoriaSwapPorcentagem = psutil.swap_memory()
+
+    #Disco
     discoPorcentagem = psutil.disk_usage('/')[3]
+
+    #Rede
     interval = 1
     statusRede = 0
     network_connections = psutil.net_connections()
 
     network_active = any(conn.status == psutil.CONN_ESTABLISHED for conn in network_connections)
+
+    #Outros
+    boot_time = datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    print(boot_time)
 
     
 
@@ -47,10 +70,11 @@ while True:
     horarioAtual = datetime.now()
     horarioFormatado = horarioAtual.strftime('%Y-%m-%d %H:%M:%S')
     
-    ins = [cpuPorcentagem, memoriaPorcentagem, discoPorcentagem, statusRede]
-    componentes = [1,2,3,4]
-    cursor = connection.cursor()
+    ins = [cpuPorcentagem, cpuVelocidadeEmGhz, tempoSistema, processos, memoriaPorcentagem, memoriaTotal, memoriaUsada, memoriaSwapPorcentagem, discoPorcentagem, statusRede]
+    componentes = [1,2,3,4,5,6,7,8,9,10]
     
+    cursor = connection.cursor()
+    """
     for i in range(len(ins)):
         
         dado = ins[i]
@@ -65,8 +89,13 @@ while True:
 
 
         connection.commit()
+       """ 
     print("\nINFORMAÇÕES SOBRE PROCESSAMENTO: ")
-    print('Porcentagem utilizada do Processador: ',cpuPorcentagem,'\nPorcentagem utilizada de memoria: ', memoriaPorcentagem,'\nPorcentagem do disco sendo utilizada:', discoPorcentagem,'\nStatus da rede: ',statusRede)
+    print('Porcentagem utilizada da CPU: ',cpuPorcentagem,
+          '\nVelocidade da CPU: ',cpuVelocidadeEmGhz,
+          '\nPorcentagem utilizada de memoria: ', memoriaPorcentagem,
+          '\nPorcentagem do disco sendo utilizada:', discoPorcentagem,
+          '\nStatus da rede: ',statusRede)
    
     
        
